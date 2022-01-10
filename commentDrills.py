5a5e7a5ba57c27ee829c8380bbb605f2eafd4d1e
GRAnguyen <97421810+GRAnguyen@users.noreply.github.com>
2022-01-09 23:33:20 -0600

    Create commentDrills.py
    1/9/22

---
+++ commentDrills.py
@@ -0,0 +1,47 @@
+'''
+For this assignment you should read the task, then below the task do what 
+it asks you to do.
+EXAMPLE TASK:
+'''
+#EX) Make a single line comment
+
+#This is a single line comment
+
+'''
+END OF EXAMPLE
+'''
+
+'''
+START HERE
+'''
+
+#1) Make a single line comment.
+
+#I am a single line comment
+
+#2) Make a multi-line comment.
+
+'''
+I
+am
+a
+multi-line
+comment
+'''
+
+#3) Make a variable. Then put a comment after it, on the same line.
+
+var = 12 #It equals twelve
+
+#4) Make a single line comment.
+
+#Have a good day
+
+#5) Make a multi-line comment.
+
+'''
+I don't know about you
+But I'm feeling 22
+Everything will be alright if
+You keep me next to you
+'''
