cdfb85bf41c4e93bbef092a5dcfb51b34ab1703e
GRAnguyen <97421810+GRAnguyen@users.noreply.github.com>
2022-02-02 21:23:26 -0600

    Update mathOutline.py

--- mathOutline.py
+++ mathOutline.py
@@ -6,7 +6,7 @@ Fill in this outline as the instructor goes through the lesson.
 #1) Make two string variables and use the + operator to combine them into
 #one new variable.
 
-x = "Grace" + "Nguyen"
+x = "Grace" + " Nguyen"
 print(x)
 
 #2) Make two int variables and use the + operator to combine them into
@@ -35,7 +35,7 @@ print(x)
 #6) Make one string variable and one int variable and use the * operator 
 #to combine them into one new variable.
 
-x = "Grace" * 3
+x = "Grace " * 3
 print(x)
 
 #7) Make two int variables and use the / operator to combine them into
@@ -70,7 +70,7 @@ print(x)
 
 #12) Make any type of operation with a common SYNTAX error
 
-y = "purple" + 5
-print(y)
+stingVar = "purple" + 5
+print(stringVar)
 
 
