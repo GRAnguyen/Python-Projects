e009c1c736c1969d83cc8ba14c172c9b5b65a52c
GRAnguyen <97421810+GRAnguyen@users.noreply.github.com>
2022-02-02 21:50:44 -0600

    Create comparisonOutline.py

---
+++ comparisonOutline.py
@@ -0,0 +1,59 @@
+'''
+This outline will help solidify concepts from the Comparison Operators lesson.
+Fill in this outline as the instructor goes through the lesson.
+'''
+
+#1) Make two string variables. Compare them using the == operator and store
+#that comparison in a new variable. Then print the variable.
+
+apples = "apples"
+oranges = "oranges"
+fruit = (apples == oranges)
+print(fruit)
+
+#2) Make two int variables. Compare them using the == operator and store
+#that comparison in a new variable. Then print the variable.
+
+a = 3
+b =7
+c = (a == b)
+print(c)
+
+#3) Make two int variables. Compare them using the > operator and store
+#that comparison in a new variable. Then print the variable.
+
+d = 8
+e = 9
+f = (d > e)
+print(f)
+
+#4) Make two int variables. Compare them using the < operator and store
+#that comparison in a new variable. Then print the variable.
+
+g = 1
+h = 2
+i = (g < h)
+print(i)
+
+#5) Make two int variables. Compare them using the >= operator and store
+#that comparison in a new variable. Then print the variable.
+
+j = 4
+k = 5
+l = (j >= k)
+print(l)
+
+#6) Make two int variables. Compare them using the <= operator and store
+#that comparison in a new variable. Then print the variable.
+
+m = 6
+n = 3
+o = (m <= n)
+print(o)
+
+#7) Make a comparison with any operator, with one of the common SYNTAX errors.
+
+string = "sun"
+weather = "clouds"
+sky = (string == wheather)
+print(sky)
